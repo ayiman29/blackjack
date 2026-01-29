bc62df037b428e65661fd8c3ab7996569e57a27f
RZ Ayiman <ayimaneg@gmail.com>
2026-01-29 18:13:13 +0600

    A complete game implemented

--- bj.py
+++ bj.py
@@ -27,23 +27,27 @@ def game(bet, cards = cards):
     player_score = 0
     dealer_score = 0
     print(f"Hello! You have placed a bet of {bet}$. Now, the dealer is picking your cards")
+    sleep(1)
     player.append(pick(cards))
     player.append(pick(cards))
     print()
     print("Your cards are:", player)
     print()
+    sleep(1)
     print("Now, the dealer is picking his cards")
     dealer.append(pick(cards))
     dealer.append(pick(cards))
     print()
-    print("The dealer's first card is", dealer[0])
+    sleep(1)
+    print(f"Dealer's cards are [{dealer[0]}, XX]")
     flag = "H"
+    player_blackjack = False
+    dealer_blackjack = False
     while flag != "S":
         player_score = 0
         for i in player:
             if i == "A":
                 player_score += 11
-                A = True
             elif i == "K" or i == "Q" or i == "J":
                 player_score += 10
             else:
@@ -55,9 +59,14 @@ def game(bet, cards = cards):
                     if player_score <= 21:
                         break
         if player_score > 21:
-            return f"You are busted... You have lost {bet}$ Better luck next time!"
+            print("Your score:", score)
+            return 0
         elif player_score == 21:
-            return f"You have WON!!! You have won {bet}$"
+            print("Your Score: 21!!!!")
+            
+            if len(player) == 2:
+                player_blackjack = True
+            break
         print("Your score:", player_score)
         flag = input("Type (H) to Hit and (S) to Stand: ").upper()
         if flag == "H":
@@ -75,15 +84,54 @@ def game(bet, cards = cards):
                 dealer_score += 10
             else:
                 dealer_score += int(i)
+
         A = dealer.count("A")
         while dealer_score > 21 and A > 0:
             dealer_score -= 10
             A -= 1
-            
+        if dealer_score == 21 and len(dealer) == 2:
+            dealer_blackjack = True
         print("Dealer Score:", dealer_score)
         if dealer_score >= 17:
             break
         dealer.append(pick(cards))
+    
+    if dealer_blackjack > 21:
+        return 1
+    if dealer_blackjack:
+        if player_blackjack:
+            return 3
+        else:
+            return 0
+    if player_blackjack:
+        return 2
 
-print(game(100000))
+    elif player_score > dealer_score:
+        return 1
+    elif dealer_score > player_score:
+        return 0
+    else:
+        return 3
+
+#0 -> Dealer wins
+#1 -> Player wins
+#2 -> Player blackjack
+#3 -> push
+
+
+bet = int(input("WELCOME TO BLACKJACK!!!! PLEASE PLACE YOUR BET: "))
+sleep(1)
+res = game(bet)
+sleep(1)
+if res == 0:
+    print("YOU HAVE LOST;((")
+    print(f"LOST: {bet}$")
+elif res == 1:
+    print("YOU HAVE WON!!!")
+    print(f"WON: {bet}$")
+elif res == 2:
+    print("$$$BLACKJACK$$$")
+    print(f"WON: {bet*(1.5)}$")
+else:
+    print("It's a Push!")
 
